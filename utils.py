174ee23eafa2d4adaf9942b475fcda293fa129db
Amogh SJ <86942531+asj06@users.noreply.github.com>
2023-04-18 07:48:59 +0530

    Reverted changes done on shareus

--- utils.py
+++ utils.py
@@ -484,10 +484,9 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://api.shareus.in/shortlink'
-    params = {'token': 'yk5WMEmc62eQE4zVEFWPungOO552',
-              'link': link,
-              'format':'json'
+    url = f'https://{URL_SHORTENR_WEBSITE}/api'
+    params = {'api': URL_SHORTNER_WEBSITE_API,
+              'url': link,
               }
 
     try:
@@ -498,7 +497,7 @@ async def get_shortlink(link):
                     return data['shortenedUrl']
                 else:
                     logger.error(f"Error: {data['message']}")
-                    return f'https:https://api.shareus.in/directLink?token={URL_SHORTNER_WEBSITE_API}&link={link}'
+                    return f'https://{URL_SHORTENR_WEBSITE}/api?api={URL_SHORTNER_WEBSITE_API}&link={link}'
 
     except Exception as e:
         logger.error(e)
